fd74182e936f19263f7ddc21ed98209a31d63f49
Léo Bernard <54135715+Yolwoocle@users.noreply.github.com>
2025-11-04 21:29:50 +0100

    Testing new W3 music

--- data/sounds.lua
+++ data/sounds.lua
@@ -25,7 +25,7 @@ local sfxnames = {
 	["music_w1_paused"] =                   { "music/music_w1_paused.ogg", "stream", { looping = true } },
 	["music_w2_ingame"] =                   { "music/music_w2_ingame.ogg", "stream", { looping = true } },
 	["music_w3_ingame"] =                   { "music/music_w3_ingame.ogg", "stream", { looping = true } },
-	["music_w3_paused"] =                   { "music/music_w3_ingame.ogg", --[[<- CHANGEME]] "stream", { looping = true } },
+	["music_w3_paused"] =                   { "music/music_w3_paused.ogg", "stream", { looping = true } },
 	["music_game_over"] =                   { "music/music_game_over.ogg", "stream", { looping = true } },
 	["music_cafeteria_ingame"] =            { "music/music_cafeteria_ingame.ogg", "stream", { looping = true } },
 	["music_cafeteria_paused"] =            { "music/music_cafeteria_paused.ogg", "stream", { looping = true } },
