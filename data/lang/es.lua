de063a86e884599b0d15e9135401f522da19bc69
Alejandro Alzate <alejandroalzate@github.com>
2026-02-11 08:58:28 -0500

    Translate boss names and credit strings

--- data/lang/es.lua
+++ data/lang/es.lua
@@ -1,12 +1,12 @@
 --[[
     TO TRANSLATORS:
-    * Reference document for all enemies, players, levels, etc: 
+    * Reference document for all enemies, players, levels, etc:
       https://docs.google.com/document/d/13UntpWqoTXgYnBm5HL0pZmjBDwMStIN8YB1IPdi7hlA
-    * Even though my target audience is people who already play games, since the game supports 
-      local co-op and has very simple, accessible controls, it's not absurd to think that more 
+    * Even though my target audience is people who already play games, since the game supports
+      local co-op and has very simple, accessible controls, it's not absurd to think that more
       occasional gamers would try their hand at the game.
     * It is very easy for me to add more glyphs if needed, just tell me and I'll do it.
-    * Please notify me if there are any special technical requirements. (e.g. text rendering specifics, etc) 
+    * Please notify me if there are any special technical requirements. (e.g. text rendering specifics, etc)
 ]]
 
 return {
@@ -24,7 +24,7 @@ return {
 	},
 	game = {
 		-- There's `demostración` but we can get away with just demo as is
-		demo = "DEMO", -- Chip added to the game logo to indicate that this version is a demo
+		demo = "DEMO", -- Chip appended to the game logo to indicate that this version is a demo
 		fps = "%d FPS",
 		congratulations = "FELICITACIONES!",
 		win_thanks = "Gracias por jugar esta demo",
@@ -85,13 +85,13 @@ return {
 		boss_1 = "Sr. estiércol", -- keeping the format but people pick up less onto the abbreviation of sir (Señor)
 		boss_2 = "Su majestad",
 		boss_3 = "El maestro de redes",
-		
-        -- A very large cabbage-like, boulder-like, rolling enemy from the Garden area.   
-        boss_4 = "Rollossus", -- ADDED
+
+        -- A very large cabbage-like, boulder-like, rolling enemy from the Garden area.
+        boss_4 = "Coloso Rodadoso",
 
         -- The CEO of the company, and the final boss. Its name is somewhat ominous-sounding.
 		-- Try to avoid ambiguity with the term "boss", which could be confused with the generic term for a video game boss.
-        boss_5 = "CEO", -- ADDED
+        boss_5 = "El Patronazo", -- works as is
 	},
 	upgrade = {
 		tea = {
@@ -280,7 +280,7 @@ return {
 				skip_boss_intros = "OMITIR INTRODUCCIÓN DE JEFES",
 				-- `Warn about low performance`
 				show_fps_warning = "AVISAR DE BAJO RENDIMIENTO", -- Whether the game should show a warning when its framerate is low
-				combo = "%d Combo",                  -- ADDED
+				combo = "%d Combo",                  -- Work as is
 			},
 			language = {
 				title = "IDIOMA",
@@ -337,7 +337,7 @@ return {
 			tv_slideshow = "Contribuciones de la presentación en la TV", -- Refers to the powerpoint TV slideshow on the title screen, which was contributed by a variety of people
 			licenses = "Recursos y Lisencias de librerías",
 			more = "Y muchos mas...",
-            thank_you_for_playing = "Thank you for playing!", -- ADDED / Shown at the end of the credits
+            thank_you_for_playing = "¡Gracias por jugar!", -- Shown at the end of the credits
 
 			x_by_y = "%s por %s", -- "ASSET_NAME by CREATOR". Used to credit assets such as sound effects
 			asset_item = "%s por %s / %s", -- "ASSET_NAME by CREATOR / LICENCE". Used to credit assets such as sound effects
@@ -346,7 +346,7 @@ return {
 			title = "Licencias de código abierto",
 		},
 	},
-	
+
 	discord = { -- Text used for Discord rich presence
 		state = {
 			solo = "Jugando solo",
